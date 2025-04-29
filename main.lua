039ddf330101dfb0812d54faedf887dba640f2bd
Agata Sumowska <iguana-admins@vitvio.com>
2025-04-29 14:09:07 +0000

    from training session

--- main.lua
+++ main.lua
@@ -1,24 +1,31 @@
+local template = require "message_template"
 -- local map = require "mappings.SIUS12"
 -- local IsFiltered = require "filter"
 
 local function ExtractSurgeryInfo(Data)
    local Out = {}
    
-   Out.MRN = Data.PATIENT_SIUS27[1].PID[2]
-	Out.PatientName = Data.PATIENT_SIUS27[1].PID[5][1]
-   Out.Duration = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[11]
-   Out.DurationUnits = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[12]
-	Out.StartDateTime = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[8]
-   Out.OrId = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[3][1]
-   Out.OrName = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[3][2]
+   Out.MRN = Data.PATIENT_SIUS27[1].PID[2]:nodeValue()
+	Out.PatientName = Data.PATIENT_SIUS27[1].PID[5][1][1][1]:nodeValue()..Data.PATIENT_SIUS27[1].PID[5][1][2]:nodeValue()
+   Out.Duration = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[11]:nodeValue()
+   Out.DurationUnits = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[12][1]:nodeValue()
+	Out.StartDateTime = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[8]:nodeValue()
+   Out.OrId = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[3][1]:nodeValue()
+   Out.OrName = Data.RESOURCES_SIUS27[1].GENERAL_RESOURCE[1].AIG[3][2]:nodeValue()
    
    return Out
 end
 
 function main(Data)
-   Data = hl7.parse{data=Data,vmd='siu.vmd'}
-   local Result = ExtractSurgeryInfo(Data)
+   local Message, Name = hl7.parse{data=Data,vmd='siu.vmd'}
+   local Result = ExtractSurgeryInfo(Message)
 
+   if Name == "CATCHALL" then
+      return
+   end
+   
+   json.parse{data=template}
+   
 	local JS = json.serialize{data=Result}
 
    queue.push{data=JS}
